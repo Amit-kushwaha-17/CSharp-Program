a0f4e7de73ad25eef68db99f40103b67e2f49a84
Amit Kushwaha <amitkush615@gmail.com>
2025-07-08 21:19:32 +0530

    Made a Method in Abstract Class and Used on Child Class

---
+++ AbstartInheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public interface IAccount
+{
+    void PayInfunds(decimal amount);
+    bool WithDrawFunds(decimal amount);
+    decimal GetBalance();
+    string RudeLetterString();
+}
+public abstract class Account : IAccount
+{
+    private decimal balance = 0;
+    public abstract string RudeLetterString();
+    public virtual bool WithDrawFunds(decimal amount)
+    {
+        if (balance < amount)
+        {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+    public void PayInfunds(decimal amount)
+    {
+        balance += amount;
+    }
+    public decimal GetBalance()
+    {
+        return balance;
+    }
+}
